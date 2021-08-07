28c4e3136e289e9ce031ba2924a79d68d4454992
JrPhy <j0958322080@gmail.com>
2021-08-07 11:06:37 +0800

    Update 2d_Gradient_Descent.c

--- Gradient Descent/2d_Gradient_Descent.c
+++ Gradient Descent/2d_Gradient_Descent.c
@@ -20,27 +20,29 @@ void twodGD(double x0, double y0, unsigned int max_iters, double lambda, double 
 {
 	double x1 = x0 + 0.1;
 	double y1 = y0 + 0.1;
-    unsigned int iters = 0;
-    double tempx, tempy ,x_error = error+1, y_error = error+1;
-    while(error < x_error && error < y_error && iters < max_iters) {
-        tempx = x1;
-        x1 -= dfx(tempx, x0, y0) * lambda;
-	    x_error = fabs(x1-tempx);
-	    tempy = y1;
-        y1 -= dfy(x1, tempy, y0) * lambda;
-	    y_error = fabs(y1-tempy);
-        printf("\nx_error %f, y_error %f\n", x_error, y_error);
-        iters++;
-        x0 = tempx;
-        y0 = tempy;
-    }
-    printf("\n iterators times %d\n", iters);
-    printf("%f	%f\n",x1, y1);
+	unsigned int iters = 0;
+	double tempx, tempy ,x_error = error+1, y_error = error+1;
+	while(error < x_error && error < y_error && iters < max_iters) 
+	{
+        	tempx = x1;
+        	x1 -= dfx(tempx, x0, y0) * lambda;
+		x_error = fabs(x1-tempx);
+		tempy = y1;
+        	y1 -= dfy(x1, tempy, y0) * lambda;
+		y_error = fabs(y1-tempy);
+		printf("\nx_error %f, y_error %f\n", x_error, y_error);
+		iters++;
+		x0 = tempx;
+		y0 = tempy;
+	}
+	printf("\n iterators times %d\n", iters);
+	printf("%f	%f\n",x1, y1);
 }
 
-int main() {
-    double lambda = 0.1, error = 10e-8;
-    int max_iters = 1000;
-    twodGD(10.0, 20.0, max_iters, lambda, error);
-    return 0;
+int main() 
+{
+	double lambda = 0.1, error = 10e-8;
+	int max_iters = 1000;
+	twodGD(10.0, 20.0, max_iters, lambda, error);
+	return 0;
 }
